7b778459a29073070066272ab846df1a1fae44bb
Léandre Bordeleau <Landre Bordeleau>
2016-10-24 18:49:49 -0400

    First commit

---
+++ app/src/main/java/com/example/salvonix/monsieur/MainActivity.java
@@ -0,0 +1,77 @@
+package com.example.salvonix.monsieur;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+   // FragmentTransaction ft;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.action_map_location:
+                showMap();
+                return true;
+            case R.id.action_settings:
+               // launchSettingsActivity();
+               // Log.d(ForecastFragment.class.getSimpleName(), "Paramètres");
+                startActivity(new Intent(this, SettingsActivity.class));
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    private void showMap() {
+        Uri geoLocation;
+
+        String QUERY_PARAM = "q";
+
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String location = sharedPrefs.getString(getString(R.string.pref_key_zipCode), getString(R.string.pref_default_zipCode));
+
+        geoLocation = Uri.parse("geo:0,0?").buildUpon()
+                .appendQueryParameter(QUERY_PARAM, location).build();
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(geoLocation);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+    /*private void launchSettingsActivity() {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
+    }*/
+
+
+}
+
